f50a8cfbfc3f5c6ec3285bc13fc41f7202c3bc70
Noah Freedley <42617616+KiritoBro2004@users.noreply.github.com>
2024-12-19 15:29:37 -0500

    NF - Initial Commit - Created main.cpp & main.h

---
+++ Neural Network/main.cpp
@@ -0,0 +1,45 @@
+// Custom Includes
+#include "main.h"
+
+#pragma region Other Includes
+#include <iostream>
+#include <map>
+#include <vector>
+#include <numeric>
+#include <thread>
+#include <mutex>
+#include <valarray>
+#include <cmath>
+#include <cstdlib>
+#include <fstream>
+#include <random>
+#include <unordered_map>
+#include <queue>
+#include <vector>
+#include <map>
+#include <cmath>
+#include <future>
+#include <iostream>
+#include <iomanip>
+#include <sstream>
+#include <ctime>
+#include <string>
+#include <filesystem>
+#include <vector>
+#include <thread>
+#include <queue>
+#include <mutex>
+#include <condition_variable>
+#include <atomic>
+#include <functional>
+#include <memory>
+#pragma endregion
+
+using namespace std;
+
+namespace Stacy {
+public:
+
+private:
+
+}
