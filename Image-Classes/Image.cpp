0fec6f467c480cf879a5df64271b65e447863c86
Nominal9977 <84743651+Nominal9977@users.noreply.github.com>
2025-09-29 14:11:29 -0400

    changes

--- Image-Classes/Image.cpp
+++ Image-Classes/Image.cpp
@@ -1,5 +1,35 @@
-//
-// Created by gking on 12/25/2024.
-//
+#include <iostream>
+#include <opencv2/highgui.hpp>
+#include "opencv2/videoio.hpp"
 
-#include "Image.h"
+using namespace cv;
+
+int main() {
+  VideoCapture cap(0);
+
+    // Check if the webcam is open
+    if(!cap.isOpened()) {
+        std::cout << "Error opening webcam!" << std::endl;
+        return -1;
+    }
+
+    namedWindow("Webcam", cv::WINDOW_AUTOSIZE);
+
+    // Create a Mat object to store each frame of the video
+    Mat frame;
+
+    // Continuously capture and display video from the webcam
+    while(1) {
+        // Capture a new frame from the webcam
+        cap >> frame;
+        // Display the frame in the window
+        cv::imshow("Webcam", frame);
+        // Wait for the user to press a key before capturing the next frame
+        if (cv::waitKey(1) >= 0) break;
+     }
+
+     // Release the webcam and destroy the window when you are done using it
+     cap.release();
+     cv::destroyWindow("Webcam");
+     return 0;
+}
